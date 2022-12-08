2260347787487da007060670c7e06312d208a2c3
Muhammad Hanif <hanifmazari2010@gmail.com>
2022-12-08 21:15:18 +0500

    inital commit

--- lib/product_list.dart
+++ lib/product_list.dart
@@ -9,44 +9,125 @@ class ProductList extends StatefulWidget {
 }
 
 class _ProductListState extends State<ProductList> {
+  List<String> productName = [
+    'Mango',
+    'Orange',
+    'Grapes',
+    'Banana',
+    'Chery',
+    'Peach',
+    'Mixed Fruit Basket',
+  ];
+  List<String> productUnit = [
+    'KG',
+    'Dozen',
+    'KG',
+    'Dozen',
+    'KG',
+    'KG',
+    'KG',
+  ];
+  List<int> productPrice = [10, 20, 30, 40, 50, 60, 70];
+  List<String> productImage = [
+    'https://image.shutterstock.com/image-photo/mango-isolated-on-white-background-600w-610892249.jpg',
+    'https://image.shutterstock.com/image-photo/orange-fruit-slices-leaves-isolated-600w-1386912362.jpg',
+    'https://image.shutterstock.com/image-photo/green-grape-leaves-isolated-on-600w-533487490.jpg',
+    'https://media.istockphoto.com/photos/banana-picture-id1184345169?s=612x612',
+    'https://media.istockphoto.com/photos/cherry-trio-with-stem-and-leaf-picture-id157428769?s=612x612',
+    'https://media.istockphoto.com/photos/single-whole-peach-fruit-with-leaf-and-slice-isolated-on-white-picture-id1151868959?s=612x612',
+    'https://media.istockphoto.com/photos/fruit-background-picture-id529664572?s=612x612',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       appBar: AppBar(
         title: Text("Product List"),
         centerTitle: true,
         actions: [
           Center(
             child: Badge(
-              badgeContent: Text("0", style: TextStyle(color: Colors.white),),
+              badgeContent: const Text(
+                "0",
+                style: TextStyle(color: Colors.white),
+              ),
               animationDuration: Duration(milliseconds: 300),
               child: Icon(Icons.shopping_bag_outlined),
-    
             ),
           ),
-          SizedBox(width: 20,)
+          SizedBox(
+            width: 20,
+          )
         ],
       ),
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
-              itemCount: 200,
-              itemBuilder:  (context, index) {
-              return Card(
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Text(index.toString())
-                      ],
-                    )
-                  ],
-                ),
-              );
-            })
-          ),
+              child: ListView.builder(
+                  itemCount: productName.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Image(
+                                    width: 100,
+                                    height: 100,
+                                    image: NetworkImage(
+                                        productImage[index].toString())),
+                                const SizedBox(
+                                      width: 10,
+                                    ),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        productName[index].toString(),
+                                        style: const TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w500),
+                                      ),
+                                      const SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text(
+                                        productUnit[index].toString()+" "+r"$" +productPrice[index].toString(),
+                                        style: const TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w500),
+                                      ),
+                                      SizedBox(height: 5,),
+                                      Align(
+                                        alignment: Alignment.centerRight,
+                                        child: Container(
+                                          
+                                          height: 35,
+                                          width: 100,
+                                          decoration: BoxDecoration(
+                                            color: Colors.green,
+                                            borderRadius: BorderRadius.circular(5)
+                                          ),
+                                          child: Center(
+                                            child: Text("Add to cart", style: TextStyle(color: Colors.white),),
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  })),
         ],
       ),
     );
